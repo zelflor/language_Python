83b7d09d74c209a4c63180cf7759ea9aecd4ced9
Zelflor (Nairukki) <139008962+zelflor@users.noreply.github.com>
2026-01-27 09:38:37 +0100

    Add files via upload

---
+++ tps/tp_01/exercice_01.py
@@ -0,0 +1,7 @@
+classeA = 32
+classeB = 27
+classeC = 34
+
+photocopies = (classeA + classeB + classeC) * 192
+
+print(photocopies)
---
+++ tps/tp_01/exercice_02.py
@@ -0,0 +1,9 @@
+responsesaisie = input("Entrez le premier nombre : ")
+responsesaisie2 = input("Entrez le deuxieme nombre : ")
+
+saisie = int(responsesaisie)
+saisie2 = int(responsesaisie2)
+
+somme = saisie * saisie2
+
+print("le produit de ces deux nombres est : ", somme ,"")
