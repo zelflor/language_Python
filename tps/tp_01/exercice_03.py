69b6e4396e101bfab85b0fafe479d27156b62aba
Zelflor (Nairukki) <139008962+zelflor@users.noreply.github.com>
2026-01-27 10:20:08 +0100

    Add files via upload

---
+++ tps/tp_01/exercice_03.py
@@ -0,0 +1,7 @@
+saisie = 6
+
+nbrCarre = saisie**2
+nbrCarre2 = saisie * saisie
+
+print("Le nombre ", saisie ," est egal a ", nbrCarre ," au carre");
+print("Le nombre ", saisie ," est egal a ", nbrCarre2 ," au carre");
---
+++ tps/tp_01/exercice_04.py
@@ -0,0 +1,7 @@
+nbrTemps = 437
+
+nbrMinutes = nbrTemps // 60
+
+nbrSecond = nbrTemps % 60
+
+print(nbrMinutes ,":", nbrSecond)
