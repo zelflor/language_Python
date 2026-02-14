b2e77564924dd693b7332c8e19acb111edaa258b
ZELFLOR <zelflor42@gmail.com>
2026-02-14 12:50:44 +0100

    Create exercice_10.py

---
+++ tps/tp_04/exercice_10.py
@@ -0,0 +1,3 @@
+# Exercice 10 : Expliquer dans quel cas on peut se passer d'évaluer la condition B dans l'expression A or B. 
+# Ecrire ensuite un programme qui permet de vérifier l'évaluation paresseuse de l'opérateur booléen or.
+
