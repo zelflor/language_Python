03f1228a61a6e8acf45ebdd6b2d1388a9f74626f
ZELFLOR <zelflor42@gmail.com>
2026-02-14 11:25:13 +0100

    Update exercice_13.py

--- tps/tp_03/exercice_13.py
+++ tps/tp_03/exercice_13.py
@@ -2,18 +2,11 @@
 # Une année est bissextile si elle est multiple de 4 mais pas multiple de 100, ou si elle est multiple de 400.
 # Tester ensuite votre programme avec les années 2021, 2020, 1900 et 2000. 
 
-# année = int(input("Veuillez saisir une année : "))
+annee = int(input("Veuillez saisir une année : "))
 
-# bissectile = False
+bissextile = (annee % 4 == 0 and annee % 100 != 0) or (annee % 400 == 0)
 
-# if (année % 4 == 0):
-#     if (année % 100 == 0):
-#         if (année % 400 == 0):
-#             bissectile = True
-#     else:
-#         bissectile = True
-
-# if (bissectile):
-#     print(f"l'année {année} est bissectile")
-# else:
-#     print(f"l'année {année} n'est pas bissectile")
+if bissextile:
+    print(f"L'année {annee} est bissextile")
+else:
+    print(f"L'année {annee} n'est pas bissextile")
