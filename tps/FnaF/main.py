7447f9b913ca407da64dff2f391f81827ff63fcb
Zelflor <139008962+zelflor@users.noreply.github.com>
2026-02-18 17:28:11 +0100

    Add files via upload

---
+++ tps/FnaF/main.py
@@ -0,0 +1,23 @@
+import os
+import threading
+import time
+import random
+from datetime import datetime
+
+class animatronique:
+    def __init__(self, name):
+        self.name = name
+
+def init_message(message):
+    print(f"{message} -------------- OK")
+
+def __init__():
+    print("Initialisation ...")
+    
+    actuel = datetime.now()
+
+    ts_actuel = actuel.timestamp()
+
+    init_message('Récupération du temps')
+
+__init__()
