536563776bef1d921d07d2867c4fdb3a2740643a
Zelflor <139008962+zelflor@users.noreply.github.com>
2026-03-16 11:26:16 +0100

    Add files via upload

---
+++ tps/tp_07/exercice_02.py
@@ -0,0 +1,9 @@
+# Exercice 2 : Modifiez la première valeur de la liste a, puis affichez la liste modifiée.
+
+a = [12,17,15,14]
+
+print(a)
+
+a[0] = 5
+
+print(a)
---
+++ tps/tp_07/exercice_03.py
@@ -0,0 +1,3 @@
+# Exercice 3 : Dans la liste ci-dessous :
+# Quel est le plus grand indice possible? 
+# Quel est le plus petit indice négatif possible? 
