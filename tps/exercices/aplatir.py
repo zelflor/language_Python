ea428dce01d88a5448e1f9a063cf4c16389c8ffd
Zelflor <139008962+zelflor@users.noreply.github.com>
2026-03-23 12:22:52 +0100

    Add files via upload

---
+++ tps/exercices/aplatir.py
@@ -0,0 +1,12 @@
+tableau = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+
+aplati = []
+
+print()
+
+for i in range(0 , len(tableau)):
+    for y in range(0, len(tableau[1])):
+        aplati.append(tableau[i][y])
+
+print(aplati)
+
